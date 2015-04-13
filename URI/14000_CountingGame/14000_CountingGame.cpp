22a436e7e77eb23f5cd0284f6b13a61146281788
Marcus Nascimento <marcus.cps@gmail.com>
2015-04-13 17:57:38 -0300

    Minor changes.

--- URI/14000_CountingGame/14000_CountingGame.cpp
+++ URI/14000_CountingGame/14000_CountingGame.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-inline bool isHit(unsigned long long int v) {
+typedef unsigned long long int ullint;
+typedef unsigned int uint;
+
+inline bool isHit(ullint v) {
 	if ((v % 7) == 0) return true;
 	while (v) {
 		if ((v % 10) == 7) return true;
@@ -22,11 +25,11 @@ int main() {
 		cin >> n >> m >> k;
 		if (n == 0 && m == 0 && k == 0) break;
 
-		unsigned int delta1 = (n - m) << 1;
-		unsigned int delta2 = (m - 1) << 1;
+		uint delta1 = (n - m) << 1;
+		uint delta2 = (m - 1) << 1;
 
-		unsigned long long int v = m;
-		unsigned long long int v0 = 0;
+		ullint v = m;
+		ullint v0 = 0;
 		while (k) {
 			if (v != v0 && isHit(v)) {
 				if (--k == 0) break;
